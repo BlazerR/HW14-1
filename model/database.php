ded58308f39c4e5d5e13b23b8200bab15be85532
BlazerR <br229@njit.edu>
2017-04-05 16:40:47 -0400

    2

--- model/database.php
+++ model/database.php
@@ -1,25 +1,13 @@
 <?php
-class Database {
-    private static $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    private static $username = 'mgs_user';
-    private static $password = 'pa55word';
-    private static $db;
+    $dsn = 'mysql:host =sql1.njit.edu;dbname=br229';
+    $username = 'br229';
+    $password = '1Rm2Gizav';
 
-    private function __construct() {}
-
-    public static function getDB () {
-        if (!isset(self::$db)) {
-            try {
-                self::$db = new PDO(self::$dsn,
-                                     self::$username,
-                                     self::$password);
-            } catch (PDOException $e) {
-                $error_message = $e->getMessage();
-                include('../errors/database_error.php');
-                exit();
-            }
-        }
-        return self::$db;
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
     }
-}
 ?>
