9b77d163dbb2b6ab4b13b6d5588393cc6028bcfd
BlazerR <br229@njit.edu>
2017-04-05 17:09:08 -0400

    5

--- model/database.php
+++ model/database.php
@@ -1,13 +1,25 @@
 <?php
-    $dsn = 'mysql:host =sql1.njit.edu;dbname=br229';
-    $username = 'br229';
-    $password = '1Rm2Gizav';
-
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('database_error.php');
-        exit();
-    }
-?>
+class Database { 
+     private static $dsn = 'mysql:host =sql1.njit.edu;dbname=br229';; 
+     private static $username = 'br229'; 
+     private static $password = '1Rm2Gizav';
+     private static $db; 
+  
+     private function __construct() {} 
+  
+     public static function getDB () { 
+         if (!isset(self::$db)) { 
+             try { 
+                 self::$db = new PDO(self::$dsn, 
+                                      self::$username, 
+                                      self::$password); 
+             } catch (PDOException $e) { 
+                 $error_message = $e->getMessage(); 
+                 include('../errors/database_error.php'); 
+                 exit(); 
+             } 
+         } 
+         return self::$db; 
+     } 
+ } 
+ ?>
